d66e31e9266f536bac72292dafefd750fcf57610
QiangRO <aroch22222@hotmail.com>
2022-05-26 07:14:03 -0400

    Quinto commit

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['usuario'])){
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Aeropuerto Juan N Álvarez</title>
+  <title>Aeropuerto Internacional de El Alto</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
